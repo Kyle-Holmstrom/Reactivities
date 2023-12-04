aa9cd8eb4bd83438d92301dee36027e516fb347c
Kyle Holmstrom <holmstrom.kyle@yahoo.com>
2023-12-04 17:13:00 -0500

    Added in a get user to the store and set up user icon in navbar

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -13,6 +13,14 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
 axios.interceptors.response.use(async res => {
     await sleep(1000);
     return res;
@@ -54,7 +62,6 @@ axios.interceptors.response.use(async res => {
     return Promise.reject(error);
 })
 
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
